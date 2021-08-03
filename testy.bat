557d26b426fc261935ff4c83ff1ca515cd34ae19
chivesz <34375681+chivesz@users.noreply.github.com>
2021-08-03 00:33:41 -0400

    Delete testy.bat

--- testy.bat
+++
@@ -1,3 +0,0 @@
-start /wait bitsadmin /transfer notepad /download /priority high https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.1.2/npp.8.1.2.Installer.x64.exe C:\notepad.exe
-start /wait C:\notepad.exe
-del C:\notepad.exe
